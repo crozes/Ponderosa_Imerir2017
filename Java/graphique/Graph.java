4b790bcbd5d59698d198e2da7dcc1ac5ff7bfc6c
briceGA <brice.guenaut-alinat@imerir.com>
2017-06-23 13:53:04 +0200

    affichage et mise a jour de l'heure et la date

--- Java/graphique/Graph.java
+++ Java/graphique/Graph.java
@@ -1,12 +1,15 @@
 package graphique;
 
 
+import java.util.ArrayList;
+
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import communication.Communication;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -27,32 +30,58 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import les_mains.MainTest;
 import outils.Global;
 
 public class Graph extends Application  {
-	String texte;
 	int nbcol=10;
 	int nblign=1;
-	public String recup(){
-	
-		texte = Communication.getRecevoir(Global.URL_TEST_JSON_GET);
-
+	Label Label2= new Label(); 
+	/**recuperation d'info depuis le serveur*/
+	////////////////////////////////////////recupMJB//////////////////////////////////////
+	/**recuperation de la meteo du jour et du budjet*/
+	private String recupMJB(){
+		String texte = Communication.getRecevoir(Global.URL_TEST_JSON_GET);
 		JsonElement jelement = new JsonParser().parse(texte);
 		JsonObject json = jelement.getAsJsonObject();
-
-
-
-		System.out.println(json.toString());
-
 		String weather = json.get("weather").getAsString();
 		int day = json.get("day").getAsInt();
 		float budget = json.get("budget").getAsFloat();
-		System.out.println("Afficher string : " + weather + " int : " + day + " float :" + budget);
 		return "meteo : " + weather + "\n jour : " + day + "\n budjet : " + budget;
 	}
-	public void gridPanel(int nbcol,int nblign,GridPane panel){
+	/////////////////////////////////////////recupNbJoueur//////////////////////////////
+	private int recupNbJoueur(){
+		String texte = Communication.getRecevoir("https://ponderosaproject.herokuapp.com/nbrPlayer");
+		JsonElement jelement = new JsonParser().parse(texte);
+		JsonObject json = jelement.getAsJsonObject();
+		int nbJoueur = json.get("nbrPlayer").getAsInt();
+		return nbJoueur;
+	
+	}
+	////////////////////////////////////////recupHour//////////////////////////////////
+	/**recuperation de l'heure*/
+	private String recupHour(){
+		String texte = Communication.getRecevoir("https://ponderosaproject.herokuapp.com/getHour");
+		JsonElement jelement = new JsonParser().parse(texte);
+		JsonObject json = jelement.getAsJsonObject();
+		int time = json.get("time").getAsInt();
+		String date=hourJour(time);
+		return date;
+	}
+	////////////////////////////////////////hourJour/////////////////////////////////////
+	/**trensformation time en jour et heure*/
+	private String hourJour(int time){
+		int jour=time/24;
+		int heure=time-(24*jour);
+		String.valueOf(jour);
+		String.valueOf(heure);
+		return jour+"jour \n\n"+heure+"h00";
+	}
+	/////////////////////////////////////////gridpanel/////////////////////////////////
+	/**crée les case du gridpane*/
+	private void gridPanel(int nbcol,int nblign,GridPane panel){
 		for (int i = 0; i < nbcol; i++) {
             ColumnConstraints colConst = new ColumnConstraints();
             colConst.setPercentWidth(100.0 / nbcol);
@@ -65,13 +94,16 @@ public class Graph extends Application  {
         }
         panel.setGridLinesVisible(true);
 	}
-
-    public void start(Stage primaryStage) {
-    	/**creation de la fenetre*/
+	/////////////////////////////////////////start///////////////////////////////////
+				/**affichage*/
+    public void start(Stage primaryStage) {  
+    	/*variable de test*/
+    	int recupNbJoueur=10;
+    	/*creation de la fenetre*/
     	primaryStage.setTitle("limonade");
         Group root=new Group();//arriere de la fenetre
         Scene scene= new Scene(root,700,700,Color.LIGHTBLUE);//scene
-        /**creation de panel*/
+        /*creation de panel*/
         VBox page=new VBox();
         GridPane upPanel = new GridPane();
         gridPanel(10,1,upPanel);
@@ -79,48 +111,77 @@ public class Graph extends Application  {
         gridPanel(2,1,genPanel);
         page.getChildren().add(upPanel);
         page.getChildren().add(genPanel);
-        /**contenu de la page*/
-        String text="meteo : \n jour : \n budjet : ";//recup();
+        /*contenu de la page*/
+        /////////////////case1////////////////
+        String MJB=recupMJB();
         Label topLabel= new Label(); 
         topLabel.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE); 
-        topLabel.setStyle("-fx-background-color: white; -fx-border-color: white;");
-        topLabel.setText(text);
+        topLabel.setStyle("-fx-background-color: white; -fx-border-color: black;");
+        topLabel.setText(MJB);
+        upPanel.add(topLabel, 0, 0);
+        /////////////////case2///////////////
+        String heure=recupHour();
         
-        ///////////////////////////////////////contenu de la page///////////////////////////////////////
+        Label2.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE); 
+        Label2.setStyle("-fx-background-color: white; -fx-border-color: black;-fx-alignment:CENTER; -fx-font-size: 20pt ;");
+        Label2.setText(heure);
+        upPanel.add(Label2, 1, 0);
+        /////////////////case5//////////////
+        ComboBox listJoueur=new ComboBox();
+        
+         ArrayList joueur=new ArrayList();
+         for(int i=0;i<=recupNbJoueur/*()*/;i++){
+        	
+     		joueur.add("joueur"+i);
+     	}
+		listJoueur.getItems().addAll(
+        	joueur
+       );
+		listJoueur.setValue("joueur");
+        upPanel.add(listJoueur, 5, 0);
+        //case L2 1
         for(int i=0;i<10;i++){
         	Circle rond=new Circle();
         	rond.setCenterX(1000f);
         	rond.setCenterY(1000f);
-        	rond.centerXProperty();
+        	rond.centerXProperty();//
         	rond.centerYProperty();
         	rond.setRadius(10*i);
         	rond.setFill(Color.rgb(100, 100, 100, 0.2));
-        	genPanel.add(rond,0, 0);
+        	genPanel.add(rond,2, 0);
         }
-        ComboBox listJoueur=new ComboBox();
-        listJoueur.getItems().addAll(
-        	"joueur 1",
-        	"joueur 2",
-        	"joueur 3"
-       );
-        upPanel.add(topLabel, 0, 0);
-        upPanel.add(listJoueur, 5, 0);
-        
-        
-        /*Circle pnj =new Circle();
-        pnj.setCenterX(1500);
-        pnj.setCenterY(1500);
-        pnj.setRadius(50);*/
-        
-        /**affichage*/
+        MAJ lol = new MAJ();
+        lol.start();
+        /*affichage*/
         
         root.getChildren().add(page);
         primaryStage.setScene(scene);
-        primaryStage.show(); 
+        primaryStage.show();
+    }
+    /////////////////////////////////threadRun///////////////////////////////////////////
+    /**met a jour l'heure et la date*/
+    private class MAJ extends Thread{
+    	public void run(){
+    		System.out.println("coucou");
+    		while(true)
+    		{
+    			try { 
+    				  Thread.sleep(1000);
+    				}
+    				catch (InterruptedException exception) {
+    				  exception.printStackTrace();
+    				}
+    			String heure=recupHour();
+    			Platform.runLater(()->Label2.setText(heure));
+    		}
+    	}
     }
     
+    //////////////////////////////////main///////////////////////////////////////////////
 	public static void main(String[] args) {
+		 
 		launch(Graph.class,args);
    }
+	
 	
 }
